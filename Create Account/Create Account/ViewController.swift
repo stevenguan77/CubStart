353057f424364eeecb285c92e722a063e7f9f4a7
Steven Guan <stevenguan@Stevens-MacBook-Air.local>
2021-03-07 23:29:05 -0800

    Create account

---
+++ Create Account/Create Account/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Create Account
+//
+//  Created by Steven Guan on 3/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var AccountInfo: UILabel!
+    
+    @IBOutlet weak var NameText: UITextField!
+    
+    @IBOutlet var EmailText: UIView!
+    @IBOutlet weak var Username: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    @IBOutlet weak var Gender: UITextField!
+    @IBOutlet weak var Age: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
